06a4b9b48a7f4db440c7e7f70896099038076ba8
Anh Le <lehoanganh.le2001@gmail.com>
2023-03-26 01:28:00 +0700

    comment schemastore

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -14,10 +14,10 @@ local generalConfigs = {
 local specificConfigs = {
   jsonls = {
     -- lazy-load schemastore when needed
-    on_new_config = function(new_config)
-      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
-      vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-    end,
+    -- on_new_config = function(new_config)
+    --   new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+    --   vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    -- end,
     settings = {
       json = {
         format = {
